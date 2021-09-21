52e39e4d4662be3b7489ff2f699e04ac78a682de
SwallowMuysua <90800550+SwallowMuysua@users.noreply.github.com>
2021-09-20 22:24:05 -0500

    Delete Pelicula.cs

--- Sprint II/Código fuente/Pelicula.cs
+++
@@ -1,9 +0,0 @@
-using System;
-namespace blazorProyI.Shared.Entidades
-{
-    public class Pelicula
-    {
-        public string Titulo {get;set;}
-        public DateTime Premier {get;set;}
-    }
-}
