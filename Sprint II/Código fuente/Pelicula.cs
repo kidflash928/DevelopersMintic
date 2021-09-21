063c8ae83423193436f9c7e8e6aca346ee444bcc
SwallowMuysua <90800550+SwallowMuysua@users.noreply.github.com>
2021-09-20 21:48:28 -0500

    Create Pelicula.cs

---
+++ Sprint II/Código fuente/Pelicula.cs
@@ -0,0 +1,9 @@
+using System;
+namespace blazorProyI.Shared.Entidades
+{
+    public class Pelicula
+    {
+        public string Titulo {get;set;}
+        public DateTime Premier {get;set;}
+    }
+}
