5edb230f6e94701d005582e7976fed97c3ae527c
SwallowMuysua <90800550+SwallowMuysua@users.noreply.github.com>
2021-09-26 20:24:39 -0500

    Add files via upload

---
+++ Sprint III/Código Fuente/Client/Program.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Peliculas.Client.Servicios;
+
+namespace Peliculas.Client
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            ConfigureServices(builder.Services);
+            await builder.Build().RunAsync();
+        }
+        private static void ConfigureServices(IServiceCollection services){
+            services.AddSingleton<IserviciosPeliculas,ServicioPelicula>();
+        }
+    }
+}
